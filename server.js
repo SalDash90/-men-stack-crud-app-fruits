ca39d6f577265d3a9e84868fe8a47e042938a732
salmandashti <salman.y.dashti@gmail.com>
2024-12-24 20:30:31 +0300

    delete/edit added

--- server.js
+++ server.js
@@ -56,6 +56,26 @@ app.delete("/fruits/:fruitId", async (req, res) => {
   res.redirect("/fruits");
 });
 
+app.get("/fruits/:fruitId/edit", async (req, res) => {
+  const foundFruit = await Fruit.findById(req.params.fruitId);
+  res.render("fruits/edit.ejs", {
+    fruit: foundFruit,
+  });
+});
+
+// server.js
+
+app.put("/fruits/:fruitId", async (req, res) => {
+  if (req.body.isReadyToEat === "on") {
+    req.body.isReadyToEat = true;
+  } else {
+    req.body.isReadyToEat = false;
+  }
+  await Fruit.findByIdAndUpdate(req.params.fruitId, req.body);
+
+  res.redirect(`/fruits/${req.params.fruitId}`);
+});
+
 
   app.listen(3000, () => {
     console.log('Listening on port 3000');
